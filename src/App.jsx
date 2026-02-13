e0314ca30e673dd599bf4f5b906226a2bf93cc5a
ljcl79 <ljcl79@gmail.com>
2026-02-13 19:32:15 -0300

    Feat: Proyecto Inicial

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+      <div>
+        <p>Celebrando con la gente de Tester Engineer</p>
+      </div>
+    </>
+  )
+}
+
+export default App
